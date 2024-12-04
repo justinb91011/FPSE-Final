b7bc915a93260478ebc95ef40b3900fcfd0677e8
Justin Bravo <justinbravo@Justins-MacBook-Pro.local>
2024-12-04 14:04:05 -0500

    pushing files

--- src/cpu.mli
+++ src/cpu.mli
@@ -1,4 +1,5 @@
 open Uno_card
+open Deck
 
 module CPU : sig
   type difficulty = Easy | Medium | Hard
@@ -19,13 +20,13 @@ module CPU : sig
   val play_card : t -> UnoCardInstance.t -> t
   (** [play_card cpu card] removes the given [card] from the CPU player's hand if it is playable. *)
 
-  val choose_card : t -> UnoCardInstance.t list -> UnoCardInstance.t
-  (** [choose_card cpu options opponents] allows the CPU to select a card to play from [options]. 
-      The decision depends on the [difficulty]:
-      - Easy: Chooses a random card.
-      - Medium: Alternates between random and algorithmic choice.
-      - Hard: Strictly uses the ranking algorithm. *)
-      (* REMOVED opponents:int list *)
+  val choose_card : t -> UnoCardInstance.t -> Deck.t -> UnoCardInstance.t * Deck.t * t
+    (** [choose_card cpu top_card deck] allows the CPU to select a card to play based on the [top_card] 
+        and [deck]. The decision depends on the [difficulty]:
+        - Easy: Chooses a random playable card or draws if none are available.
+        - Medium: Alternates between random and algorithmic choice.
+        - Hard: Strictly uses the ranking algorithm. *)
+    
 
   val has_won : t -> bool
   (** [has_won cpu] returns [true] if the CPU player has no cards left, [false] otherwise. *)
