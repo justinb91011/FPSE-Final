d8eeaab95fb0fa83a6a643cfcff1fffc74a6d22e
Justin Bravo <justinbravo@Justins-MacBook-Pro.local>
2024-12-06 21:45:57 -0500

    DrawTwo is now implemented

--- src/run_game.ml
+++ src/run_game.ml
@@ -62,10 +62,8 @@ let handle_skip_card state played_card =
   let value = UnoCardInstance.get_value played_card in
   match value with
   | Skip ->
-    (* Skip the next player's turn by advancing once more *)
     { state with current_player_index = next_player_index state }
   | _ ->
-    (* If not a skip card, do nothing *)
     state
 
 (* Function to handle the reverse card logic *)
@@ -73,41 +71,116 @@ let handle_reverse_card state played_card who_played =
   let value = UnoCardInstance.get_value played_card in
   match value with
   | Reverse ->
-    (* Determine new direction and next turn based on who played and current direction *)
     let new_direction, new_turn =
       if state.direction = 1 then
-        (* Currently clockwise *)
         match who_played with
-        | 0 (* Player1 played Reverse *) -> (-1, 2)  (* direction = -1, turn = CPU#2 (index 2) *)
-        | 1 (* CPU1 played Reverse *) -> (-1, 0)    (* direction = -1, turn = Player1 (index 0) *)
-        | 2 (* CPU2 played Reverse *) -> (-1, 1)    (* direction = -1, turn = CPU#1 (index 1) *)
+        | 0 -> (-1, 2)
+        | 1 -> (-1, 0)
+        | 2 -> (-1, 1)
         | _ -> (state.direction, state.current_player_index)
       else
-        (* Currently counterclockwise (direction = -1) *)
         match who_played with
-        | 0 (* Player1 played Reverse *) -> (1, 1)  (* direction = 1, turn = CPU#1 (index 1) *)
-        | 1 (* CPU1 played Reverse *) -> (1, 2)     (* direction = 1, turn = CPU#2 (index 2) *)
-        | 2 (* CPU2 played Reverse *) -> (1, 0)     (* direction = 1, turn = Player1 (index 0) *)
+        | 0 -> (1, 1)
+        | 1 -> (1, 2)
+        | 2 -> (1, 0)
         | _ -> (state.direction, state.current_player_index)
     in
     { state with direction = new_direction; current_player_index = new_turn }
   | _ ->
-    (* If not a reverse card, do nothing *)
     state
 
-(* Modified play_cpu_turn to return (new_state, card, cpu_index) *)
+(* Helper functions to reconstruct player/CPU after removing a card *)
+let remove_card_from_player p card =
+  let hand = Player.get_hand p in
+  let filtered = List.filter hand ~f:(fun c -> not (UnoCardInstance.equal c card)) in
+  let name = Player.get_name p in
+  let p' = Player.create name in
+  Player.add_cards p' filtered
+
+let remove_card_from_cpu c card =
+  let hand = CPU.get_hand c in
+  let filtered = List.filter hand ~f:(fun cc -> not (UnoCardInstance.equal cc card)) in
+  let diff = CPU.get_difficulty c in
+  let c' = CPU.create diff in
+  CPU.add_cards c' filtered
+
+(* DrawTwo logic *)
+let handle_draw_two state played_card =
+  let value = UnoCardInstance.get_value played_card in
+  match value with
+  | DrawTwo ->
+    let rec resolve_draw_two state stack =
+      let current_index = state.current_player_index in
+      let is_player = (current_index = 0) in
+      let current_hand =
+        if is_player then
+          Player.get_hand (snd (List.hd_exn state.players))
+        else
+          CPU.get_hand (snd (List.nth_exn state.cpus (current_index - 1)))
+      in
+      let draw_two_cards = List.filter current_hand ~f:(fun c ->
+        match UnoCardInstance.get_value c with DrawTwo -> true | _ -> false
+      ) in
+
+      if List.is_empty draw_two_cards then
+        (* No DrawTwo -> draw stack cards and skip turn *)
+        let (drawn_cards, new_deck) = Deck.draw_cards stack state.deck in
+        let state =
+          if is_player then
+            let (pname, p) = List.hd_exn state.players in
+            let p = Player.add_cards p drawn_cards in
+            { state with players = [(pname, p)]; deck = new_deck }
+          else
+            let i = current_index - 1 in
+            let (cname, c) = List.nth_exn state.cpus i in
+            let c = CPU.add_cards c drawn_cards in
+            let cpus = List.mapi state.cpus ~f:(fun idx (nm, cc) ->
+              if idx = i then (cname, c) else (nm, cc)
+            ) in
+            { state with cpus; deck = new_deck }
+        in
+        let state = { state with current_player_index = next_player_index state } in
+        let player_name_str =
+          if is_player then "Player1"
+          else if current_index = 1 then "CPU1" else "CPU2"
+        in
+        (state, Some (Printf.sprintf "%s drew %d cards and turn skipped." player_name_str stack))
+      else
+        (* They have a DrawTwo -> stack it *)
+        let chosen = List.hd_exn draw_two_cards in
+        let state =
+          if is_player then
+            let (pname, p) = List.hd_exn state.players in
+            let p' = remove_card_from_player p chosen in
+            { state with
+              players = [(pname, p')];
+              discard_pile = chosen :: state.discard_pile
+            }
+          else
+            let i = current_index - 1 in
+            let (cname, c) = List.nth_exn state.cpus i in
+            let c' = remove_card_from_cpu c chosen in
+            let cpus = List.mapi state.cpus ~f:(fun idx (nm, cc) ->
+              if idx = i then (cname, c') else (nm, cc)
+            ) in
+            { state with cpus; discard_pile = chosen :: state.discard_pile }
+        in
+        let stack = stack + 2 in
+        let state = { state with current_player_index = next_player_index state } in
+        resolve_draw_two state stack
+    in
+    resolve_draw_two state 2
+  | _ -> (state, None)
+
 let play_cpu_turn state =
   let cpu_index = state.current_player_index in
   let (_, current_cpu) = List.nth_exn state.cpus (cpu_index - 1) in
   let top_discard = List.hd_exn state.discard_pile in
 
-  (* CPU chooses a card *)
   let card, new_deck, updated_cpu = CPU.choose_card current_cpu top_discard state.deck in
 
-  (* Determine if CPU played the card *)
   let card_played = not (List.exists (CPU.get_hand updated_cpu) ~f:(UnoCardInstance.equal card)) in
 
-  (* Update discard pile only if card was actually played *)
   let new_discard_pile =
     if card_played then
       card :: state.discard_pile
@@ -167,7 +240,6 @@ let () =
                          (UnoCardInstance.get_color card) (UnoCardInstance.get_value card)
                          (UnoCardInstance.get_color top_discard) (UnoCardInstance.get_value top_discard)
                     then
-                      (* Play the chosen card *)
                       let player = Player.play_card player card top_discard in
                       let discard_pile = card :: state.discard_pile in
                       let state = { state with
@@ -177,30 +249,34 @@ let () =
                       } in
                       game_state := Some state;
 
-                      (* Handle skip card if played *)
                       let state = handle_skip_card (Option.value_exn !game_state) card in
                       game_state := Some state;
 
-                      (* Player index who played is always 0 here *)
                       let state = handle_reverse_card (Option.value_exn !game_state) card 0 in
                       game_state := Some state;
 
-                      (* Check if player has won after handling skip and reverse *)
+                      let (state, draw_msg) = handle_draw_two (Option.value_exn !game_state) card in
+                      game_state := Some state;
+
                       let _, player = List.hd_exn state.players in
                       if Player.has_won player then
                         Dream.html "Player1 has won the game. Game over."
-                      else 
-                        Dream.html (Printf.sprintf "Card played successfully!")
+                      else
+                        (* If draw_msg is Some, print both lines, else just "Card played successfully!" *)
+                        (match draw_msg with
+                         | Some m ->
+                           Dream.html (Printf.sprintf "Card played successfully!\n%s" m)
+                         | None ->
+                           Dream.html "Card played successfully!")
                     else
                       Dream.html ~code:400 "Card is not playable. Please choose a different card."
                 end
               else
-                (* No playable cards in hand, draw one from the deck *)
+                (* No playable cards, draw one *)
                 let (drawn_card, new_deck) = Deck.draw_card state.deck in
                 let player = Player.add_cards player [drawn_card] in
                 let state = { state with deck = new_deck } in
 
-                (* Describe the drawn card *)
                 let drawn_color = UnoCardInstance.get_color drawn_card in
                 let drawn_value = UnoCardInstance.get_value drawn_card in
                 let drawn_card_str =
@@ -213,7 +289,6 @@ let () =
                      (UnoCardInstance.get_color drawn_card) (UnoCardInstance.get_value drawn_card)
                      (UnoCardInstance.get_color top_discard) (UnoCardInstance.get_value top_discard)
                 then
-                  (* Play the drawn card *)
                   let player = Player.play_card player drawn_card top_discard in
                   let discard_pile = drawn_card :: state.discard_pile in
                   let state = { state with
@@ -223,32 +298,36 @@ let () =
                   } in
                   game_state := Some state;
 
-                  (* Handle skip if the drawn card played is skip *)
                   let state = handle_skip_card (Option.value_exn !game_state) drawn_card in
                   game_state := Some state;
 
-                  (* Player played so who_played=0 *)
                   let state = handle_reverse_card (Option.value_exn !game_state) drawn_card 0 in
                   game_state := Some state;
 
-                  (* Check if player has won after possibly skipping and reversing *)
+                  let (state, draw_msg) = handle_draw_two (Option.value_exn !game_state) drawn_card in
+                  game_state := Some state;
+
                   let _, player = List.hd_exn state.players in
                   if Player.has_won player then
                     Dream.html "Player1 has won the game. Game over."
                   else
-                    let top_discard = List.hd_exn state.discard_pile in
-                    let top_color = UnoCardInstance.get_color top_discard in
-                    let top_value = UnoCardInstance.get_value top_discard in
-                    let top_card_str =
-                      Printf.sprintf "%s %s"
-                        (Sexp.to_string (UnoCard.sexp_of_color top_color))
-                        (Sexp.to_string (UnoCard.sexp_of_value top_value))
-                    in
-                    Dream.html (Printf.sprintf
-                      "No playable card in your hand; you drew %s and played it! Top card: %s"
-                      drawn_card_str top_card_str)
+                    (match draw_msg with
+                     | Some m ->
+                       (* Print both messages *)
+                       Dream.html (Printf.sprintf "Card played successfully!\n%s" m)
+                     | None ->
+                       let top_discard = List.hd_exn state.discard_pile in
+                       let top_color = UnoCardInstance.get_color top_discard in
+                       let top_value = UnoCardInstance.get_value top_discard in
+                       let top_card_str =
+                         Printf.sprintf "%s %s"
+                           (Sexp.to_string (UnoCard.sexp_of_color top_color))
+                           (Sexp.to_string (UnoCard.sexp_of_value top_value))
+                       in
+                       Dream.html (Printf.sprintf
+                         "No playable card in your hand; you drew %s and played it! Top card: %s"
+                         drawn_card_str top_card_str))
                 else
-                  (* Keep the drawn card and next turn *)
                   let state = { state with
                     players = [(player_name, player)];
                     current_player_index = 1;
@@ -258,7 +337,6 @@ let () =
                     "No playable card in your hand; you drew %s and kept it. Turn ends."
                     drawn_card_str));
 
-    (* Handle the game state *)
     Dream.get "/" (fun _ ->
       match !game_state with
       | None -> Dream.html "Game not initialized."
@@ -288,7 +366,6 @@ let () =
           player_name hand_str top_card_str)
     );
 
-    (* Handle the CPU turns *)
     Dream.post "/cpu_turn" (fun _ ->
       match !game_state with
       | None -> Dream.html "Game not initialized."
@@ -301,18 +378,19 @@ let () =
 
           let new_top = List.hd_exn new_state.discard_pile in
           if UnoCardInstance.equal chosen_card new_top then
-            (* CPU played the chosen_card *)
             let state = handle_skip_card (Option.value_exn !game_state) chosen_card in
             game_state := Some state;
 
-            (* CPU played, who_played = cpu_index (1 or 2) *)
             let state = handle_reverse_card (Option.value_exn !game_state) chosen_card cpu_index in
             game_state := Some state;
-    
+
+            let (state, draw_msg) = handle_draw_two (Option.value_exn !game_state) chosen_card in
+            game_state := Some state;
+
             let (_, updated_cpu) = List.nth_exn state.cpus (cpu_index - 1) in
             if CPU.has_won updated_cpu then
               Dream.html (Printf.sprintf "CPU%d has won the game. Game over." cpu_index)
-            else 
+            else
               let played_color = UnoCardInstance.get_color chosen_card in
               let played_value = UnoCardInstance.get_value chosen_card in
               let played_card_str =
@@ -320,9 +398,12 @@ let () =
                   (Sexp.to_string (UnoCard.sexp_of_color played_color))
                   (Sexp.to_string (UnoCard.sexp_of_value played_value))
               in
-              Dream.html (Printf.sprintf "CPU%d turn completed. Played: %s" cpu_index played_card_str)
+              (match draw_msg with
+               | Some m ->
+                 Dream.html (Printf.sprintf "CPU%d turn completed. Played: %s\n%s" cpu_index played_card_str m)
+               | None ->
+                 Dream.html (Printf.sprintf "CPU%d turn completed. Played: %s" cpu_index played_card_str))
           else
-            (* CPU did not play the card, only drew it *)
             Dream.html (Printf.sprintf "CPU%d turn completed. Drew a card." cpu_index)
     );
 
